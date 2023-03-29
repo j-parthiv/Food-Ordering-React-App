509913a460ca82f03287de6af9141afefe7b1686
j-parthiv <parthivjani9@gmail.com>
2023-03-29 15:45:34 +0200

    Manage Cart and Modal State

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
 import Cart from "./Components/Cart/Cart";
+
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler = () =>{
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler = () =>{
+    setCartIsShown(false);
+  }
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -2,12 +2,12 @@ import { Fragment } from "react";
 import mealsImage from "../../Assets/meals.jpg";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
-const Header = () => {
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton/>
+        <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of deliicious food!" />
