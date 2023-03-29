03916f54c0976e375bba82a252508cbb82abb902
j-parthiv <parthivjani9@gmail.com>
2023-03-29 11:31:46 +0200

    Working on the Shopping Cart Component

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,21 @@
+import classes from './Cart.module.css';
+
+const Cart = (props) =>{
+    const cartItem = <ul className={classes.['cart-items']}>{[{id:'c1', name:'Sushi', amount:2, price: 12.99}].map((item)=><li>{item.name}</li>)}</ul>
+return(
+    <div>
+        {cartItems}
+        <div className={classes.total}>
+            <span>Total Amount</span>
+            <span>$35.62</span>
+        </div>
+        <div className={classes.actions}>
+            <button className={classes['button--alt']}>Close</button>
+            <button className={classes.button}>Order</button>
+        </div>
+        <div></div>
+    </div>
+)
+}
+
+export default Cart;
--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 const MealItem = (props) =>{
     const price = `$${props.price}`
@@ -10,7 +11,7 @@ return(
             <div className={classes.price}>{price}</div>
         </div>
         <div>
-
+            <MealItemForm id={props.id}/>
         </div>
     </li>
 )
