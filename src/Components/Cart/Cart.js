1944ced9fe04d4328951ee9082ca0606e781e563
j-parthiv <parthivjani9@gmail.com>
2023-04-10 13:08:51 +0200

    Remove hardcoded meal  data and shift to fire base

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,9 +1,9 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
-import Modal from '../UI/Modal';
-import CartItem from './CartItem';
-import classes from './Cart.module.css';
-import CartContext from '../Store/cart-context';
+import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import classes from "./Cart.module.css";
+import CartContext from "../Store/cart-context";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
@@ -12,23 +12,25 @@ const Cart = (props) => {
   const hasItems = cartCtx.items.length > 0;
 
   const cartItemRemoveHandler = (id) => {
-    cartCtx.removeItem(id)
+    cartCtx.removeItem(id);
   };
 
   const cartItemAddHandler = (item) => {
-    cartCtx.addItem({...item, amount: 1});
+    cartCtx.addItem({ ...item, amount: 1 });
   };
 
   const cartItems = (
-    <ul className={classes['cart-items']}>
+    <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
         <CartItem
           key={item.id}
           name={item.name}
           amount={item.amount}
           price={item.price}
-          onRemove={cartItemRemoveHandler.bind(null, item.id)}
-          onAdd={cartItemAddHandler.bind(null, item)}
+          onRemove={()=>cartItemRemoveHandler(item.id)}
+          //onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          //onAdd= {cartItemAddHandler.bind(null, item)}
+          onAdd= {()=>cartItemAddHandler(item)}
         />
       ))}
     </ul>
@@ -42,7 +44,7 @@ const Cart = (props) => {
         <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
         {hasItems && <button className={classes.button}>Order</button>}
